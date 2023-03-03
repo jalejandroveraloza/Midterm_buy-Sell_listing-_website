adc94f4d05e3e42b21adf53e92db36a68545be56
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2023-03-02 21:07:41 -0800

    adding info to the database

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,9 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, phone, email, address, is_admin)
+VALUES
+('Andre Moura', '255 555 554', 'andremoura@gmail.com', 'Winterfell Road, Winterfell', true),
+('Jorge Vera', '444 555 1222', 'jorgevera@gmail.com', 'Kingslanding road, Kingslanding', true),
+('Eli Colussi', '552 443 3232', 'elicolussi@gmail.com', 'The Wall road, The Wall', true),
+('Tyrion Lannister', '332 332 1122', 'tyrionlannister@alwayspaysdebts.com', 'Casterly Rock Road', false);
+
+
