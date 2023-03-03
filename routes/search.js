d7fc6d5713328cf67512f42b2f6af93e396ff01b
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2023-03-02 21:24:25 -0800

    adding search route for prices

---
+++ routes/search.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+
+  // Search page to filter products by price
+  router.get("/search", (req, res) => {
+    let queryString = `SELECT * FROM users WHERE is_admin = true;
+    SELECT * FROM products `;
+
+    if (req.query.minPrice && req.query.maxPrice) {
+      queryString += `WHERE price >= ${req.query.minPrice} AND price <= ${req.query.maxPrice}; `;
+    } else if (req.query.minPrice) {
+      queryString += `WHERE price >= ${req.query.minPrice}`;
+    } else if (req.query.maxPrice) {
+      queryString += `WHERE price <= ${req.query.maxPrice}`;
+    }
+
+    db.query(queryString)
+    .then(data => {
+      const user = req.session.user_id;
+      const adminData = data.rows[0];
+      const products = data.rows.slice(1);
+      const templateVars = { products: products, currentUser: user, admin: adminData }
+      res.render("search", templateVars)
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+  });
+
+  return router;
+}
